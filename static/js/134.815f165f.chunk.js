fe7cd3aa37c942010ac65a5684d0a85ee08e70c5
SandraKoch <sandraannakoch@gmail.com>
2023-07-31 22:29:16 +0000

    Deploying to gh-pages from @ SandraKoch/goit-react-hw-05-movies@7bbcfca6743ca4690d849553c92e75f1a96f55c8 🚀

--- static/js/134.815f165f.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkgoit_react_hw_05_movies=self.webpackChunkgoit_react_hw_05_movies||[]).push([[134],{134:function(e,t,i){i.r(t);var s=i(184);t.default=function(){return(0,s.jsx)("div",{children:"test"})}}}]);
-//# sourceMappingURL=134.815f165f.chunk.js.map
---
+++ static/js/134.e9c13db4.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_05_movies=self.webpackChunkgoit_react_hw_05_movies||[]).push([[134],{134:function(e,t,i){i.r(t);var r=i(184);t.default=function(){return(0,r.jsx)("div",{children:(0,r.jsxs)("form",{children:[(0,r.jsx)("input",{type:"text",placeholder:"Type the movie name here..."}),(0,r.jsx)("button",{type:"submit",children:"Search"})]})})}}}]);
+//# sourceMappingURL=134.e9c13db4.chunk.js.map
